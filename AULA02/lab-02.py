bfea16c40a0b5e6812f4efc33e2f58fbf532a229
oliveiraandre2002 <andreluizdeoliveira971299114@gmail.com>
2026-09-06 20:33:17 -0300

    lab02: entrega

---
+++ AULA02/lab-02.py
@@ -0,0 +1,15 @@
+#validador_transacoes.py
+
+transacoes = [150.0, 3200.5, 12500.0, 450.0, -50.0, 800.0, 0]
+
+for transacao in transacoes:
+
+    if transacao > 10000.00:
+        print(f"[ALERTA] Transação suspeita de R$ {transacao:.2f}: Encaminhada para auditoria.")
+        continue
+
+    if transacao <= 0:
+        print(f"[ERRO CRÍTICO] Transação inválida encontrada (R$ {transacao:.2f}). Interrompendo bot...")
+        break
+
+    print(f"[SUCESSO] Transação de R$ {transacao:.2f} processada.")
